69e738ccd78860ec82e5728367113167eaacc3ae
Sergey S. Smirnov <SergeySSmirnov@mail.ru>
2017-02-23 23:17:23 +0300

    Added config file + refactoring.
    Signed-off-by: Sergey S. Smirnov <SergeySSmirnov@mail.ru>

--- classes/Message/Core.php
+++ classes/Message/Core.php
@@ -1,23 +1,16 @@
-<?php defined('SYSPATH') or die('No direct script access.'); 
+<?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Message is a class that lets you easily send messages of different types in same time in your application (aka Flash Messages).
  * @package Message
- * @author Sergey S. Smirnov 
+ * @author Sergey S. Smirnov
  * @author Dave Widmer
  * @see https://github.com/SergeySSmirnov/kohana-message
  * @see http://github.com/daveWid/message
  * @copyright  2010-2011 © Dave Widmer
  * @copyright  2014-2016 © Sergey S. Smirnov
  */
-class Message_Core
-{
-	/**
-	 * The default view if one isn't passed into display/render.
-	 * @var string
-	 */
-	public static $default = "message/basic";
+class Message_Core {
 
-	
 	/**
 	 * Type of message: ERROR.
 	 * @var string
@@ -39,64 +32,31 @@ class Message_Core
 	 */
 	const WARN = 'warn';
 
-	
 	/**
-	 * The messages to display.
-	 * @var array
-	 */
-	public $message = array();
-	/**
-	 * The type of message.
+	 * The default view if one isn't passed into display/render.
 	 * @var string
 	 */
-	public $type;
-
-	
+	public static $default = "message/basic";
 	/**
-	 * Creates a new Message instance.
-	 * @param string Type of message.
-	 * @param string|array The message(s) to display.
+	 * Config of module.
+	 * @var Config_Group
 	 */
-	public function __construct($type, $message) {
-		$this->type = $type;
-		$this->message = (array)$message;
-	}
+	protected static $config = null;
 
-	
+
 	/**
 	 * Clears the message from the session.
 	 */
 	public static function clear() {
-		Session::instance()->delete('flash_message');
+		self::getSession()->delete(self::$config['session_key']);
 	}
 	/**
-	 * Displays the message.
-	 * @param string Name of the view.
-	 * @return string Message(s) to string (HTML).
-	 */
-	public static function display($view = null) {
-		$_html = "";
-		if($_msg = self::get()) {
-			self::clear();
-			$_html = View::factory(Text::getVal($view, self::$default))->set('messages', $_msg)->render();
-		}
-		return $_html;
-	}
-	/**
-	 * The same as display - used to mold to Kohana standards.
-	 * @param string Name of the view.
-	 * @return string Message(s) to string (HTML).
-	 */
-	public static function render($view = null) {
-		return self::display($view);
-	}
-	/**
-	 * Gets all messages (if not defined param) or the messages of defined in param type. 
+	 * Gets all messages (if not defined param) or the messages of defined in param type.
 	 * @param string $type The type of message or get messages of all types.
 	 * @return Message|array The message or array of messages.
 	 */
-	public static function get($type = null) {
-		$_selfMess = Session::instance()->get('flash_message', array());
+	public static function get(string $type = null) {
+		$_selfMess = self::getSession()->get(self::$config['session_key'], array());
 		return (!empty($type) && isset($_selfMess[$type])) ? $_selfMess[$type] : $_selfMess;
 	}
 	/**
@@ -104,15 +64,15 @@ class Message_Core
 	 * @param string Type of message.
 	 * @param string|array String or array of string for the message.
 	 */
-	public static function set($type, $message) {
+	public static function set(string $type, $message) {
 		$_messages = self::get();
-		if ( ! (isset($_messages[$type]) && ($_messages[$type] instanceof Message)))
+		if (!(isset($_messages[$type]) && ($_messages[$type] instanceof Message)))
 			$_messages[$type] = new Message($type, $message);
 		elseif (is_array($message))
 			$_messages[$type]->message = array_merge($_messages[$type]->message, $message);
 		else
 			$_messages[$type]->message[] = $message;
-		Session::instance()->set('flash_message', $_messages);
+		self::getSession()->set(self::$config['session_key'], $_messages);
 	}
 	/**
 	 * Sets an error message.
@@ -141,6 +101,53 @@ class Message_Core
 	 */
 	public static function warn($message) {
 		self::set(Message::WARN, $message);
+	}
+	/**
+	 * Render the message block.
+	 * @param string Name of the view.
+	 * @return string Message(s) to string (HTML).
+	 */
+	public static function render(string $view = null) : string {
+		$_html = "";
+		if($_msg = self::get()) {
+			self::clear();
+			$_html = View::factory(Text::getVal($view, self::$default))->set('messages', $_msg)->render();
+		}
+		return $_html;
+	}
+
+
+	/**
+	 * Load current module config and return current user session.
+	 * @return Session Current user session.
+	 */
+	protected static function getSession() : Session {
+		if(!isset(self::$config))
+			self::$config = Kohana::$config->load('message');
+			return Session::instance(self::$config['session_type']);
+	}
+
+
+	/**
+	 * The messages to display.
+	 * @var array
+	 */
+	public $message = array();
+	/**
+	 * The type of message.
+	 * @var string
+	 */
+	public $type;
+
+
+	/**
+	 * Creates a new Message instance.
+	 * @param string Type of message.
+	 * @param string|array The message(s) to display.
+	 */
+	protected function __construct($type, $message) {
+		$this->type = $type;
+		$this->message = (array)$message;
 	}
 
 }
---
+++ config/message.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'session_type' => 'native', // Session type: native, database, ...
+	'session_key' => 'FLASH_MSGS' // Key in session that contains data of messages
+);
